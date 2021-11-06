5f15f110fe95d87e1844c8a7100e2bfdc5c460e7
rajasekhar1404 <mrrajasekhar09@gmail.com>
2021-11-06 11:04:13 +0530

    my sample java programs

---
+++ GuessTheCorrectNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class miniproject {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int myNumber = (int) (Math.random()*50);
+    int userNumber = 0;
+    do {
+        System.out.println("Guess my number(1-50): ");
+        userNumber = sc.nextInt();
+        if (userNumber == myNumber) {
+            System.out.println("that correct man");
+            break;
+        }
+        else if(userNumber > myNumber) {
+            System.out.println("your number is too high bro");
+        }
+        else {
+            System.out.println("your number is too low bro");
+        }
+    } while (userNumber >= 0);
+    System.out.print("my number was: ");
+    System.out.println(myNumber);
+        }
+    }
